b61849737cbe21821c5f787be6c0fd4d60bb565a
heqingbao <qingbao.ho@gmail.com>
2023-11-25 23:55:20 +0800

    setState是异步调用

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,11 @@ import robots from './mokedata/robots.json';
 import Robot from './components/Robot';
 import ShoppingCart from './components/ShoppingCart';
 
-interface Props {}
+interface Props { }
 
 interface State {
   roboGallery: any,
+  count: number,
 }
 class App extends React.Component<Props, State> {
 
@@ -17,13 +18,14 @@ class App extends React.Component<Props, State> {
     super(props);
     this.state = {
       roboGallery: [],
+      count: 0,
     }
   }
 
   componentDidMount(): void {
-      fetch("https://jsonplaceholder.typicode.com/users")
+    fetch("https://jsonplaceholder.typicode.com/users")
       .then((resp) => resp.json())
-      .then((data) => this.setState({roboGallery: data}))
+      .then((data) => this.setState({ roboGallery: data }))
   }
 
   render() {
@@ -33,6 +35,31 @@ class App extends React.Component<Props, State> {
           <img src={logo} className={styles.appLogo} alt="logo" />
           <h1>罗伯特机器人炫酷吊炸天online购物平台的名字要长</h1>
         </div>
+        <button onClick={()=>{
+          this.setState({
+            count: this.state.count + 1
+          }, ()=>{
+            console.log(this.state.count)
+          })
+          console.log(this.state.count);
+
+          // 调用两次，还是一次效果
+          this.setState({
+            count: this.state.count + 1
+          })
+          this.setState({
+            count: this.state.count + 1
+          })
+
+          // 如果要生效两次，这样使用
+          this.setState((preState, preProps) =>{
+            return {count: preState.count + 1}
+          })
+          this.setState((preState, preProps) =>{
+            return {count: preState.count + 1}
+          })
+        }}>Click</button>
+        <span>count: {this.state.count}</span>
         <ShoppingCart />
         <div className={styles.robotList}>
           {this.state.roboGallery.map((r) => (
