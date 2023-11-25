74666c6660a4848f340d57bdb0430aca14658c1a
heqingbao <qingbao.ho@gmail.com>
2023-11-25 22:44:13 +0800

    媒体与字体使用

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './assets/images/logo.svg';
 // import './App.css';
 import styles from './App.module.css';
 import robots from './mokedata/robots.json';
@@ -8,6 +8,10 @@ import Robot from './components/Robot';
 function App() {
   return (
     <div className={styles.app}>
+      <div className={styles.appHeader}>
+        <img src={logo} className={styles.appLogo} alt="logo"/>
+        <h1>罗伯特机器人炫酷吊炸天online购物平台的名字要长</h1>
+      </div>
       <div className={styles.robotList}>
         {robots.map((r) => (
           <Robot id={r.id} name={r.name} email={r.email} />
