771cdd615ee6646a8efb8b6808efbba73e61aeae
heqingbao <qingbao.ho@gmail.com>
2023-11-25 19:48:55 +0800

    JSS模块化引入组件

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import styles from './App.module.css';
 import robots from './mokedata/robots.json';
 import Robot from './components/Robot';
 
 function App() {
   return (
-    <ul>
-      { robots.map((r) => (
-        <Robot id={r.id} name={r.name} email={r.email} />
-      )) }
-    </ul>
+    <div className={styles.app}>
+      <div className={styles.robotList}>
+        {robots.map((r) => (
+          <Robot id={r.id} name={r.name} email={r.email} />
+        ))}
+      </div>
+    </div>
   );
 }
 
